b308e59de7457cd32ba67eaa41f4c12d00e091db
Masaya Kazama <miyan@t-p.jp>
2022-05-03 10:28:49 +0900

    秒数表示を切り下げに修正

--- src/components/ThePomo.vue
+++ src/components/ThePomo.vue
@@ -43,7 +43,7 @@ const workLabel = parseParamString(params.worklabel) ?? "work";
 const breakLabel = parseParamString(params.breaklabel) ?? "break";
 
 const formedTime = computed(() => {
-  const intLeftTime = Math.round(timeLeft.value / 1000);
+  const intLeftTime = Math.floor(timeLeft.value / 1000);
   const mm = String(Math.floor(intLeftTime / 60));
   const ss = String(Math.floor(intLeftTime % 60));
 
